2dd05c73d73f986cbae3213b83e9e789e4c5b858
Jimmy <jshen35@gmail.com>
2021-12-06 00:59:53 -0500

    Finish looks

--- popup.js
+++ popup.js
@@ -1,12 +1,20 @@
+const validSite = "pso2.jp\/players\/catalog\/(ac|sg|sp)scratch\/([0-9]{8}_[0-9]{2})\/"
+var scratchID = ""
+var scratchType = ""
+
 document.addEventListener("DOMContentLoaded", function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         var activeTab = tabs[0];
-        if (validWebsite(activeTab.url)) {
+        if (GetScratchID(activeTab.url)) {
             $('#itemlistButton').on("click", onClick_DownloadItemListJson)
             $('#bonuslistButton').on("click", onClick_DownloadBonusListJson)
             $('#itemImageButton').on("click", onClick_DownloadItemImages)
             $('#bonusImageButton').on("click", onClick_DownloadBonusItemImages)
             $('#allImageButton').on("click", onClick_DownloadAllItemImages)
+            $('#itemNamelistButton').on("click", onClick_DownloadItemName)
+            $('#bonusNamelistButton').on("click", onClick_DownloadBonusName)
+            console.log(scratchType)
+            $(`#${scratchType}Scratch`).show()
         } else {
             $('#validWebsite').hide()
             $('#invalidWebsite').show()
@@ -14,19 +22,24 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 })
 
-const validSite = "pso2.jp\/players\/catalog\/(ac|sg|sp)scratch\/([0-9]{8}_[0-9]{2})\/"
-
-function validWebsite(url) {
+function GetScratchID(url) {
     var itemRegex = "item"
+    var regexMatch = url.match(validSite)
 
-    return url.match(validSite) != null && url.match(itemRegex) == null
+    if (regexMatch == null && url.match(itemRegex) == null)
+        return false
+
+    scratchType = regexMatch[1]
+    scratchID = `${regexMatch[1]}_${regexMatch[2]}`
+
+    return true
 }
 
 function onClick_DownloadItemListJson() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        var activeTab = tabs[0];
+        let activeTab = tabs[0];
         console.log(tabs[0])
-        var itemlistJson = new URL("js/itemlist.json", activeTab.url).href
+        let itemlistJson = new URL("js/itemlist.json", activeTab.url).href
         chrome.downloads.download({
             url: itemlistJson
         });
@@ -35,9 +48,9 @@ function onClick_DownloadItemListJson() {
 
 function onClick_DownloadBonusListJson() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        var activeTab = tabs[0];
+        let activeTab = tabs[0];
         console.log(tabs[0])
-        var itemlistJson = new URL("js/bonuslist.json", activeTab.url).href
+        let itemlistJson = new URL("js/bonuslist.json", activeTab.url).href
         chrome.downloads.download({
             url: itemlistJson
         });
@@ -102,23 +115,49 @@ function DownloadItemImages(jsonName) {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         var activeTab = tabs[0];
         var jsonUrl = new URL(`js/${jsonName}.json`, activeTab.url).href
-        var regexMatch = activeTab.url.match("pso2.jp\/players\/catalog\/(ac|sg|sp)scratch\/([0-9]{8}_[0-9]{2})\/")
-        var scratchID = `${regexMatch[1]}_${regexMatch[2]}`
         $.ajax({
             url: jsonUrl,
             type: "GET",
             dataType: "json"
         }).then(data => {
-            var imageList = getImageList(activeTab.url, data)
-            var zip = JSZip()
+            let imageList = getImageList(activeTab.url, data)
+            let zip = JSZip()
 
             imageList.forEach(image => {
                 zip.file(image[0], urlToPromise(image[1]), { binary: true });
             })
 
             zip.generateAsync({ type: "blob" }).then(function callback(blob) {
-                saveAs(blob, `${scratchID}_${jsonName}.zip`);
+                saveAs(blob, `${scratchID}_${jsonName}_images.zip`)
             });
+        })
+    })
+}
+
+function onClick_DownloadItemName() {
+    jsonToNameList("itemlist")
+}
+
+function onClick_DownloadBonusName() {
+    jsonToNameList("bonuslist")
+}
+
+function jsonToNameList(jsonName) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        var activeTab = tabs[0];
+        var jsonUrl = new URL(`js/${jsonName}.json`, activeTab.url).href
+        $.ajax({
+            url: jsonUrl,
+            type: "GET",
+            dataType: "json"
+        }).then(data => {
+            let list = []
+            data.forEach(elem => {
+                list.push(elem.name)
+            })
+            let blob = new Blob([list.join("\n")], { type: "text/plain;charset=utf-8" })
+            saveAs(blob, `${scratchID}_${jsonName}_names.txt`);
+            return list
         })
     })
 }
